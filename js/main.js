29a87c65585d8c615671f46e5c7a9c38b0ab4474
Shawn Gao <xgao@xgao-GA-MA78G-DS3H.(none)>
2014-11-03 21:31:54 +0800

    Add textarea
    Add shortcut to textarea to create new sub item.

--- js/main.js
+++ js/main.js
@@ -21,6 +21,19 @@
       });
     };
 
+    $scope.onKeyDown = function(scope, $event) {
+      console.log($event);
+      console.log($event.ctrlKey);
+      console.log($event.altKey);
+      console.log($event.shiftKey);
+      console.log($event.keyCode);
+      if ($event.shiftKey && 13 == $event.keyCode) {
+        scope.newSubItem(scope);
+        //$event.cancelBubble = true;
+        $event.returnValue = false;
+      }
+    }
+
     $scope.tree1 = [{
       "id": 1,
       "title": "tree1 - item1",
