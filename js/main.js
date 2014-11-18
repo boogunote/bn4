a88fca00ab843ba493283a8ccca465f6cc31dc09
Boogu <boogunote@gmail.com>
2014-11-18 23:34:10 +0800

    Move functions from main.js to nodeCtrl.js
    Functions for node operations should be put in nodeCtrls.js, not in
    main.js.

--- js/main.js
+++ js/main.js
@@ -4,36 +4,6 @@
   angular.module('treesApp', ['ui.tree'])
   .controller('treesCtrl', function($scope) {
 
-    $scope.remove = function(scope) {
-      scope.remove();
-    };
-
-    $scope.toggle = function(scope) {
-      scope.toggle();
-    };
-
-    $scope.newSubItem = function(scope) {
-      var nodeData = scope.$modelValue;
-      nodeData.nodes.push({
-        id: nodeData.id * 10 + nodeData.nodes.length,
-        title: nodeData.title + '.' + (nodeData.nodes.length + 1),
-        nodes: []
-      });
-    };
-
-    $scope.onKeyDown = function(scope, $event) {
-      console.log($event);
-      console.log($event.ctrlKey);
-      console.log($event.altKey);
-      console.log($event.shiftKey);
-      console.log($event.keyCode);
-      if ($event.shiftKey && 13 == $event.keyCode) {
-        scope.newSubItem(scope);
-        //$event.cancelBubble = true;
-        $event.returnValue = false;
-      }
-    }
-
     $scope.tree1 = [{
       "id": 1,
       "title": "tree1 - item1",
--- js/tree/controllers/nodeCtrl.js
+++ js/tree/controllers/nodeCtrl.js
@@ -174,6 +174,28 @@
           return subDepth;
         };
 
+        $scope.newSubItem = function(scope) {
+          var nodeData = scope.$modelValue;
+          nodeData.nodes.push({
+            id: nodeData.id * 10 + nodeData.nodes.length,
+            title: nodeData.title + '.' + (nodeData.nodes.length + 1),
+            nodes: []
+          });
+        };
+
+        $scope.onKeyDown = function(scope, $event) {
+          console.log($event);
+          console.log($event.ctrlKey);
+          console.log($event.altKey);
+          console.log($event.shiftKey);
+          console.log($event.keyCode);
+          if ($event.shiftKey && 13 == $event.keyCode) {
+            scope.newSubItem(scope);
+            //$event.cancelBubble = true;
+            $event.returnValue = false;
+          }
+        };
+
       }
     ]);
 })();
