d76eaed525d7665685ac2af05eef07ea41ba7f3e
Boogu <boogunote@gmail.com>
2014-12-07 01:13:25 +0800

    [Feature] Note list can show name.

--- js/list.js
+++ js/list.js
@@ -14,12 +14,39 @@
       $scope.noteId = "note1";
       $scope.base_url = "https://boogu.firebaseio.com/" + $scope.username + "/" + $scope.app_name;
       $scope.list_url = $scope.base_url + "/note_list";
-      console.log($scope.base_url)
       var noteList = $firebase(new Firebase($scope.list_url)).$asObject();
       noteList.$loaded().then(function() {
       	console.log(noteList)
       	//$scope.note_list = noteList;
       	noteList.$bindTo($scope, "note_list").then(function() {
+      	});
+      });
+    } else {
+      window.location.replace("login.html")
+    }
+  })
+  .controller('noteItemCtrl', function($scope, $firebase) {
+  	if(!$scope.note_file) return;
+
+  	var ref = new Firebase("https://boogu.firebaseio.com");
+    var authData = ref.getAuth();
+    if (authData) {
+      $scope.aaa = $scope.note_file.id
+  	  $scope.username = authData.uid;
+      $scope.app_name = "boogunote";    
+      $scope.noteId = "note1";
+      $scope.base_url = "https://boogu.firebaseio.com/" + $scope.username + "/" + $scope.app_name;
+      $scope.note_item_info_url = $scope.base_url + "/notes/" + $scope.note_file.id + "/info";
+      console.log($scope.note_item_info_url)
+      var note_item_info = $firebase(new Firebase($scope.note_item_info_url)).$asObject();
+      note_item_info.$loaded().then(function() {
+      	console.log(note_item_info)
+      	//$scope.aaa = note_item_info.$value
+      	//$scope.note_list = noteList;
+      	note_item_info.$bindTo($scope, "info").then(function() {
+      		// console.log($scope.note_item_name)
+      		// $scope.note_item = {};
+      		// $scope.note_item.name = $scope.note_item_name
       	});
       });
     } else {
